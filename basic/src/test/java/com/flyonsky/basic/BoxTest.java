1353cd1b67fd0690d55bc11197ce56e37bdc8af5
luowengang <luowengang@ky-tech.com.cn>
2019-10-28 20:19:30 +0800

    装箱拆箱测试

--- basic/src/test/java/com/flyonsky/basic/BoxTest.java
+++ basic/src/test/java/com/flyonsky/basic/BoxTest.java
@@ -1,5 +1,6 @@
 package com.flyonsky.basic;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -16,33 +17,41 @@ public class BoxTest {
         Integer a0 = new Integer(1);
         Integer a1 = new Integer(1);
         System.out.println("Integer 对象作比较 a0 == a1: " + (a0 == a1));
+        Assert.assertFalse(a0 == a1);
 
         // 调用intValue方法得到其int值
         System.out.println("使用intValue得到int值作比较 a0 == a1: " + (a0.intValue() == a1.intValue()));
+        Assert.assertTrue(a0.intValue() == a1.intValue());
 
         // 把Integer类型的变量拆箱成int类型
         int a2 = 1;
         System.out.println("将Integer自动拆箱 a1 == a2: " + (a1 == a2));
+        Assert.assertTrue(a1 == a2);
+        Assert.assertTrue(a2 == a1);
 
         // Integer对象赋值比较，其实也是内存地址的比较
         // 自动装箱，如果在-128到127之间，则值存在常量池中
         Integer a3 = 30;
         Integer a4 = 30;
         System.out.println("Integer对象赋值比较 a3 == a4: " + (a3 == a4));
+        Assert.assertTrue(a3 == a4);
 
         // Integer对象赋值(超过-128~127区间)比较
         Integer a5 = 128;
         Integer a6 = 128;
         System.out.println("Integer对象赋值(超过-128~127区间)比较 a5 == a6: " + (a5 == a6));
+        Assert.assertFalse(a5 == a6);
 
         // Integer对象赋值(超过-128~127区间)比较,调用intValue后再作比较
         Integer a7 = 128;
         Integer a8 = 128;
         System.out.println("Integer对象赋值(超过-128~127区间)比较,调用intValue后 a7 == a8: " + (a7.intValue() == a8.intValue()));
+        Assert.assertTrue(a7.intValue() == a8.intValue());
 
         // 使用Integer类的equals()方法进行的是数值的比较
         Integer a9 = 129;
         Integer a10 = 129;
         System.out.println("Integer类的equals()方法进行的是数值的比较 a9 == a10: " + a9.equals(a10));
+        Assert.assertTrue(a9.equals(a10));
     }
 }
