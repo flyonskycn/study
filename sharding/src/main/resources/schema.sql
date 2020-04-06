c2b0f29bd70ce9f61ca4f590f17fd3d4c0d9c46d
luowengang <flyonskycn@gmail.com>
2020-04-06 10:16:40 +0800

    sharding

---
+++ sharding/src/main/resources/schema.sql
@@ -0,0 +1,35 @@
+CREATE TABLE t_user
+(
+	id BIGINT(20) PRIMARY KEY auto_increment COMMENT '主键ID',
+	name VARCHAR(30) NULL DEFAULT NULL COMMENT '姓名',
+	age INT(11) NULL DEFAULT NULL COMMENT '年龄',
+	email VARCHAR(50) NULL DEFAULT NULL COMMENT '邮箱'
+);
+
+create table t_order0
+(
+  order_id BIGINT(20) not null auto_increment primary key,
+  user_id BIGINT(20) not null,
+  name varchar(100)
+);
+
+create table t_order1
+(
+  order_id BIGINT(20) not null auto_increment primary key,
+  user_id BIGINT(20) not null,
+  name varchar(100)
+);
+
+CREATE TABLE `t_order_item0` (
+  `order_id` BIGINT(20) NOT NULL,
+  `item` varchar(100) DEFAULT NULL,
+  `user_id` BIGINT(20) NOT NULL,
+  PRIMARY KEY (`order_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `t_order_item1` (
+  `order_id` BIGINT(20) NOT NULL,
+  `item` varchar(100) DEFAULT NULL,
+  `user_id` BIGINT(20) NOT NULL,
+  PRIMARY KEY (`order_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
