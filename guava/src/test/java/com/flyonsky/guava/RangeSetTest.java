b5ac8a54f2f8a3ffee7dd8385db8d31ba0602128
luowengang <luowengang@ky-tech.com.cn>
2021-02-08 15:06:48 +0800

    guava样例

--- guava/src/test/java/com/flyonsky/guava/RangeSetTest.java
+++ guava/src/test/java/com/flyonsky/guava/RangeSetTest.java
@@ -1,5 +1,7 @@
 package com.flyonsky.guava;
 
+import com.flyonsky.JsonBaseTest;
+import com.google.common.collect.DiscreteDomain;
 import com.google.common.collect.Range;
 import com.google.common.collect.RangeSet;
 import com.google.common.collect.TreeRangeSet;
@@ -10,28 +12,33 @@ import org.junit.Test;
  * @author luowengang
  * @date 2020/9/24 22:08
  */
-public class RangeSetTest {
+public class RangeSetTest extends JsonBaseTest {
 
     @Test
     public void testRangeSet(){
         RangeSet<Integer> rangeSet = TreeRangeSet.create();
         // {[1, 10]}
         rangeSet.add(Range.closed(1, 10));
-        System.out.println(rangeSet);
+        printString(rangeSet.toString());
+        printString(rangeSet.complement().toString());
+        printString(rangeSet.subRangeSet(Range.closed(5,15)).toString());
 
         // disconnected range: {[1, 10], [11, 15)}
         rangeSet.add(Range.closedOpen(11, 15));
-        System.out.println(rangeSet);
+        printString(rangeSet.toString());
         // connected range; {[1, 10], [11, 20)}
         rangeSet.add(Range.closedOpen(15, 20));
-        System.out.println(rangeSet);
+        printString(rangeSet.toString());
+
 
         // empty range; {[1, 10], [11, 20)}
         rangeSet.add(Range.openClosed(0, 0));
-        System.out.println(rangeSet);
+        printString(rangeSet.toString());
 
         // splits [1, 10]; {[1, 5], [10, 10], [11, 20)}
         rangeSet.remove(Range.open(5, 10));
-        System.out.println(rangeSet);
+        printString(rangeSet.toString());
+
+        printString("span():" + rangeSet.span().toString());
     }
 }
