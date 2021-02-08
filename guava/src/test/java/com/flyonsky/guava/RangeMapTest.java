1415b10eb3f6e381d22941f481880c7f90161562
luowengang <luowengang@ky-tech.com.cn>
2021-02-08 15:40:02 +0800

    guava样例

--- guava/src/test/java/com/flyonsky/guava/RangeMapTest.java
+++ guava/src/test/java/com/flyonsky/guava/RangeMapTest.java
@@ -1,5 +1,6 @@
 package com.flyonsky.guava;
 
+import com.flyonsky.JsonBaseTest;
 import com.google.common.collect.Range;
 import com.google.common.collect.RangeMap;
 import com.google.common.collect.TreeRangeMap;
@@ -10,28 +11,28 @@ import org.junit.Test;
  * @author luowengang
  * @date 2020/9/24 22:15
  */
-public class RangeMapTest {
+public class RangeMapTest extends JsonBaseTest {
 
     @Test
     public void testRangeMap(){
         RangeMap<Integer, String> rangeMap = TreeRangeMap.create();
         // {[1, 10] => "foo"}
         rangeMap.put(Range.closed(1, 10), "foo");
-        System.out.println(rangeMap);
+        printString(rangeMap.toString());
 
         // {[1, 3] => "foo", (3, 6) => "bar", [6, 10] => "foo"}
         rangeMap.put(Range.open(3, 6), "bar");
-        System.out.println(rangeMap);
+        printString(rangeMap.toString());
 
         // {[1, 3] => "foo", (3, 6) => "bar", [6, 10] => "foo", (10, 20) => "foo"}
         rangeMap.put(Range.open(10, 20), "foo");
-        System.out.println(rangeMap);
+        printString(rangeMap.toString());
 
         // {[1, 3] => "foo", (3, 5) => "bar", (11, 20) => "foo"}
         rangeMap.remove(Range.closed(5, 11));
-        System.out.println(rangeMap);
+        printString(rangeMap.toString());
 
-        System.out.println(rangeMap.get(15));
+        printString(rangeMap.get(15));
 
         Assert.assertEquals("foo",rangeMap.get(15));
     }
