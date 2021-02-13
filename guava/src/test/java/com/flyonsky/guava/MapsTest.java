3847370b650364f1371ff027abbbfd68b28267f5
luowengang <flyonskycn@gmail.com>
2021-02-13 18:23:57 +0800

    jstack 样例

--- guava/src/test/java/com/flyonsky/guava/MapsTest.java
+++ guava/src/test/java/com/flyonsky/guava/MapsTest.java
@@ -1,5 +1,6 @@
 package com.flyonsky.guava;
 
+import com.flyonsky.JsonBaseTest;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.MapDifference;
 import com.google.common.collect.Maps;
@@ -11,7 +12,7 @@ import java.util.Map;
  * @author luowengang
  * @date 2020/9/24 23:27
  */
-public class MapsTest {
+public class MapsTest extends JsonBaseTest {
 
     @Test
     public void testMaps(){
@@ -19,12 +20,12 @@ public class MapsTest {
         Map<String, Integer> right = ImmutableMap.of("b", 2, "c", 4, "d", 5);
         MapDifference<String, Integer> diff = Maps.difference(left, right);
         // {"b" => 2}
-        System.out.println(diff.entriesInCommon());
+        printString(diff.entriesInCommon());
         // {"c" => (3, 4)}
-        System.out.println(diff.entriesDiffering());
+        printString(diff.entriesDiffering().toString());
         // {"a" => 1}
-        System.out.println(diff.entriesOnlyOnLeft());
+        printString(diff.entriesOnlyOnLeft());
         // {"d" => 5}
-        System.out.println(diff.entriesOnlyOnRight());
+        printString(diff.entriesOnlyOnRight());
     }
 }
