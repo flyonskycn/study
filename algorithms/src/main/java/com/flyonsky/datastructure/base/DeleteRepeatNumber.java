2dfccf6e6859a32d655b84a8f7455b444e5d1517
luowengang <luowengang@ky-tech.com.cn>
2021-05-22 18:56:03 +0800

    时间空间复杂度练习

--- algorithms/src/main/java/com/flyonsky/datastructure/base/DeleteRepeatNumber.java
+++ algorithms/src/main/java/com/flyonsky/datastructure/base/DeleteRepeatNumber.java
@@ -30,6 +30,7 @@ public class DeleteRepeatNumber {
 
     public static void main(String[] args) {
         int[] nums = {0,1,1,2,3,4,4,5,5,6,7,8,8,8,9};
+//        int[] nums = {0};
 
         int len = delteRepeat(nums);
         System.out.println(len);
