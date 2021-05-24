c3bcc32b491fedc28277f9ca62975cae1a91d685
luowengang <luowengang@ky-tech.com.cn>
2021-05-24 09:18:35 +0800

    时间空间复杂度练习

--- algorithms/src/main/java/com/flyonsky/datastructure/base/RotateArray.java
+++ algorithms/src/main/java/com/flyonsky/datastructure/base/RotateArray.java
@@ -7,13 +7,14 @@ package com.flyonsky.datastructure.base;
 public class RotateArray {
 
     public static void main(String[] args){
-        int[] nums = {1,2,3,4,5,6,7};
+//        int[] nums = {1,2,3,4,5,6,7};
+        int[] nums = {1,2};
         int k = 3;
 
 //        int[] nums = {-1,-100,3,99};
 //        int k = 2;
 
-        rotate(nums,3);
+        rotate(nums,k);
         for(int i=0;i<nums.length;i++){
             System.out.println(nums[i]);
         }
@@ -26,36 +27,23 @@ public class RotateArray {
      *
      * 输入：nums = [-1,-100,3,99], k = 2
      * 输出：[3,99,-1,-100]
+     * 时间复杂度O(n)，空间复杂度O(n)
      * @param nums
      * @param k
      * @return
      */
     public static void rotate(int[] nums, int k){
         if(nums != null
-                && nums.length > k
                 && k > 0){
-            int nIndex = 0;
-            int oldIndex = 0;
-            int changCount = nums.length/k;
-            if(nums.length % k > 0){
-                changCount++;
+            int newIndex = 0;
+            int[] tmpNums = new int[nums.length];
+            for(int i=0;i<nums.length;i++){
+                tmpNums[i] = nums[i];
             }
 
-            int switchOld = 0;
-            int switchNew = 0;
-            for(int i=0; i < k; i++){
-                oldIndex = nums.length - 1 - i;
-                switchOld = nums[oldIndex];
-
-                for(int j = 1;j <= changCount; j++){
-                    nIndex = (oldIndex + k) % nums.length;
-                    switchNew = nums[nIndex];
-                    nums[nIndex] = switchOld;
-
-                    /** 新值为下一次的旧值 **/
-                    oldIndex = nIndex;
-                    switchOld = switchNew;
-                }
+            for(int i = 0;i<nums.length;i++){
+                newIndex = (i + k) % nums.length;
+                nums[newIndex] = tmpNums[i];
             }
         }
     }
