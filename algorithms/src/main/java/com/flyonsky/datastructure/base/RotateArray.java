a3726e356dfdd41106a2eaf3e407075c8fc78334
luowengang <luowengang@ky-tech.com.cn>
2021-05-31 19:50:49 +0800

    时间空间复杂度练习

--- algorithms/src/main/java/com/flyonsky/datastructure/base/RotateArray.java
+++ algorithms/src/main/java/com/flyonsky/datastructure/base/RotateArray.java
@@ -7,14 +7,15 @@ package com.flyonsky.datastructure.base;
 public class RotateArray {
 
     public static void main(String[] args){
-//        int[] nums = {1,2,3,4,5,6,7};
-        int[] nums = {1,2};
+        int[] nums = {1,2,3,4,5,6,7};
+//        int[] nums = {1,2};
         int k = 3;
 
 //        int[] nums = {-1,-100,3,99};
 //        int k = 2;
 
-        rotate(nums,k);
+//        rotate(nums,k);
+        rotateInPlace(nums, k);
         for(int i=0;i<nums.length;i++){
             System.out.println(nums[i]);
         }
@@ -44,6 +45,37 @@ public class RotateArray {
             for(int i = 0;i<nums.length;i++){
                 newIndex = (i + k) % nums.length;
                 nums[newIndex] = tmpNums[i];
+            }
+        }
+    }
+
+    /**
+     * 给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+     * 输入: nums = [1,2,3,4,5,6,7], k = 3
+     * 输出: [5,6,7,1,2,3,4]
+     *
+     * 输入：nums = [-1,-100,3,99], k = 2
+     * 输出：[3,99,-1,-100]
+     * 时间复杂度O(n)，空间复杂度O(1)
+     * @param nums
+     * @param k
+     * @return
+     */
+    public static void rotateInPlace(int[] nums,int k){
+        if(nums != null
+                && k > 0){
+            k = k % nums.length;
+            if(k == 1){
+                int tmp = nums[nums.length - 1];
+                for(int i = nums.length-2; i>=0; i--){
+                    nums[i + 1] = nums[i];
+                    if(i == 0){
+                        nums[i] = tmp;
+                    }
+                }
+            }
+            for(int i = 0; i < k; i++){
+                rotateInPlace(nums, 1);
             }
         }
     }
