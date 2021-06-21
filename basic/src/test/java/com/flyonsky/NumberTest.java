912097afb0600ef1134d0c6f540ba52623871145
luowengang <flyonskycn@gmail.com>
2021-06-21 21:29:49 +0800

    冒泡排序

--- basic/src/test/java/com/flyonsky/NumberTest.java
+++ basic/src/test/java/com/flyonsky/NumberTest.java
@@ -12,7 +12,9 @@ public class NumberTest {
     public void testDouble(){
         Double value = Double.parseDouble("12345678.6");
         System.out.println(value);
-        value = Double.parseDouble("1234567.6");
-        System.out.println(value);
+        Double value1 = Double.parseDouble("1234567.6");
+        System.out.println(value1);
+
+        System.out.println(value+value1);
     }
 }
